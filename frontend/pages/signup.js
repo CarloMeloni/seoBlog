b3ec287ecb0ba369f2e96295d2794bee7ef81dd5
carloMeloni <carlo.meloni@hotmail.it>
2020-10-07 19:17:20 +0200

    Implementing Signup form

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,12 +1,11 @@
 import Layout from '../components/Layout';
+import SignupComponent from '../components/auth/SignupComponent';
 import Link from 'next/link';
 
 const Signup = () => 
         <Layout>
             <h2>Signup.js</h2>
-            <Link href='/'>
-                <a>home</a>
-            </Link>
+            <SignupComponent />
         </Layout>;
 
 export default Signup;
