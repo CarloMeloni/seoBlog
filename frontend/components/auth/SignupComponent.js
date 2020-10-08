4d9ce9bee9d5c308b336cefb6cfe114a1619cb05
carloMeloni <carlo.meloni@hotmail.it>
2020-10-08 10:25:01 +0200

    Signup action

--- frontend/components/auth/SignupComponent.js
+++ frontend/components/auth/SignupComponent.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
+import { signup } from '../../actions/auth';
 
 const SignupComponent = () => {
     const [values, setValues] = useState({
         name: 'Carlo',
-        email: "carlo@example.com",
+        email: 'carlo@example.com',
         password: '123456',
         error: '',
         loading: false,
@@ -11,45 +12,73 @@ const SignupComponent = () => {
         showForm: true
     });
 
-    const {
-        name,
-        email,
-        password,
-        error,
-        loading,
-        message,
-        showForm
-    } = values;
+    const { name, email, password, error, loading, message, showForm } = values;
 
-    const handleSubmit = (e) => {
+    const handleSubmit = e => {
         e.preventDefault();
-        console.table({
-            name,
-            email,
-            password,
-            error,
-            loading,
-            message,
-            showForm
+        // console.table({ name, email, password, error, loading, message, showForm });
+        setValues({ ...values, loading: true, error: false });
+        const user = { name, email, password };
+
+        signup(user).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error, loading: false });
+            } else {
+                setValues({
+                    ...values,
+                    name: '',
+                    email: '',
+                    password: '',
+                    error: '',
+                    loading: false,
+                    message: data.message,
+                    showForm: false
+                });
+            }
         });
     };
 
-    const handleChange = name => (e) => {
-        setValues({ ...values, error: false, [name]: e.target.value })
+    const handleChange = name => e => {
+        setValues({ ...values, error: false, [name]: e.target.value });
     };
 
-    const SignupForm = () => {
+    const showLoading = () => (loading ? <div className="alert alert-info">Loading...</div> : '');
+    const showError = () => (error ? <div className="alert alert-danger">{error}</div> : '');
+    const showMessage = () => (message ? <div className="alert alert-info">{message}</div> : '');
+
+    const signupForm = () => {
         return (
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
-                    <input value={name} onChange={handleChange('name')} type="text" className="form-control" placeholder="Type here your name" />
+                    <input
+                        value={name}
+                        onChange={handleChange('name')}
+                        type="text"
+                        className="form-control"
+                        placeholder="Type your name"
+                    />
                 </div>
+
                 <div className="form-group">
-                    <input value={email} onChange={handleChange('email')} type="email" className="form-control" placeholder="Type here your e-mail" />
+                    <input
+                        value={email}
+                        onChange={handleChange('email')}
+                        type="email"
+                        className="form-control"
+                        placeholder="Type your email"
+                    />
                 </div>
+
                 <div className="form-group">
-                    <input value={password} onChange={handleChange('password')} type="password" className="form-control" placeholder="Type here your password" />
+                    <input
+                        value={password}
+                        onChange={handleChange('password')}
+                        type="password"
+                        className="form-control"
+                        placeholder="Type your password"
+                    />
                 </div>
+
                 <div>
                     <button className="btn btn-primary">Signup</button>
                 </div>
@@ -59,10 +88,12 @@ const SignupComponent = () => {
 
     return (
         <React.Fragment>
-            {SignupForm()}
+            {showError()}
+            {showLoading()}
+            {showMessage()}
+            {showForm && signupForm()}
         </React.Fragment>
-        
-    )
-}
+    );
+};
 
 export default SignupComponent;
