cde38cea8e7d002f7cafe84c0ebebbea2c823314
carloMeloni <carlo.meloni@hotmail.it>
2020-10-08 15:45:32 +0200

    Saving the User and the Token in cookie and localStorage

--- frontend/actions/auth.js
+++ frontend/actions/auth.js
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-fetch';
+import cookie from 'js-cookie';
 import { API } from '../config';
 
 export const signup = user => {
@@ -30,4 +31,65 @@ export const signin = user => {
             return response.json();
         })
         .catch(err => console.log(err));
+};
+
+
+//SET COOKIE
+
+export const setCookie = (key, value) => {
+    if(process.browser) {
+        cookie.set(key, value, {
+            expires: 1
+        })
+    }
+};
+
+export const removeCookie = (key) => {
+    if(process.browser) {
+        cookie.remove(key, {
+            expires: 1
+        })
+    }
+};
+
+//GET COOKIE 
+
+export const getCookie = (key) => {
+    if(process.browser) {
+        cookie.get(key);
+    }
+};
+
+//LOCALSTORAGE
+
+export const setLocalStorge = (key, value) => {
+    if(process.browser) {
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+};
+
+export const removeLocalStorge = (key) => {
+    if(process.browser) {
+        localStorage.removeItem(key);
+    }
+};
+
+//AUTHENTICATE USER BY PASS DATA TO COOKIE AND LOCALSTORAGE 
+export const authenticate = (data, next) => {
+    setCookie('token', data.token);
+    setLocalStorge('user', data.user);
+    next();
+};
+
+export const isAuth = () => {
+    if(process.browser) {
+        const cookieChecked = getCookie(token);
+        if(cookieChecked) {
+            if(localStorage.getItem('userr')) {
+                return JSON.parse(localStorage.getItem('user'));
+            } else {
+                return false;
+            }
+        }
+    }
 };
