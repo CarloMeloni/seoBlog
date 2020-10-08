378634c6077b941eb01d2eb3e3bda53694cff69b
carloMeloni <carlo.meloni@hotmail.it>
2020-10-08 10:56:03 +0200

    Signin and redirecting to the home page

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,12 +1,15 @@
 import Layout from '../components/Layout';
+import SigninComponent from '../components/auth/SigninComponent';
 import Link from 'next/link';
 
 const Signin = () => 
-        <Layout>
-            <h2>Signin.js</h2>
-            <Link href='/'>
-                <a>home</a>
-            </Link>
-        </Layout>;
+    <Layout>
+        <h2 className="text-center pt-4 pb-4">Signin</h2>
+        <div className="row">
+            <div className="col-md-6 offset-md-3">
+                <SigninComponent />
+            </div>
+        </div>
+    </Layout>;
 
 export default Signin;
